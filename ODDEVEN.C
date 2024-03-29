1b6b0661b08739f33c86d463fcee4afdbd5284a5
priya-thiyagu <priyathiaygu0908@gmail.com>
2024-03-29 14:31:22 +0530

    oddoreven

---
+++ ODDEVEN.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+int value;
+clrscr();
+printf("\ninput:");
+scanf("%d",&value);
+if(value%2 == 0)
+{
+  printf("it is even");
+}
+else
+{
+ printf("it is odd");
+ }
+ sleep(10);
+ }
